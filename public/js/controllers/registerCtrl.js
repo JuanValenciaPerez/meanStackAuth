cfe30f5cddca3f534cea1eaebb57ce1ccadb0174
philippschulte <philipp.schulte@ymail.com>
2016-07-15 20:10:17 -0600

    Add register form and functionality

--- public/js/controllers/registerCtrl.js
+++ public/js/controllers/registerCtrl.js
@@ -1,11 +1,30 @@
-(function() {
+(function () {
 
   angular
     .module('meanApp')
     .controller('registerCtrl', registerCtrl);
 
-    function registerCtrl () {
-      console.log('Register controller is running');
-    }
+  registerCtrl.$inject = ['$location', 'authentication'];
+  function registerCtrl($location, authentication) {
+    var vm = this;
+
+    vm.credentials = {
+      name : "",
+      email : "",
+      password : ""
+    };
+
+    vm.onSubmit = function () {
+      console.log('Submitting registration');
+      authentication
+        .register(vm.credentials)
+        .error(function(err){
+          alert(err);
+        })
+        .then(function(){
+          $location.path('profile');
+        });
+    };
+  }
 
 })();
